555be17a7961d982fc3233ca5d21838ef7125929
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2024-02-08 20:03:22 -0700

    hello

--- database/ironman.sql
+++ database/ironman.sql
@@ -1,11 +1,29 @@
 -- Insert a client into the clients table --
-INSERT INTO clients (clientFirstname, clientLastname, clientEmail, clientPassword) 
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password) 
 VALUES('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
--- Modify the Tony Stark record to change the clientLevel to 3 --
-UPDATE clients SET clientLevel  = 3 WHERE clientFirstname = 'Tony' AND
-clientLastname = 'Stark';
+-- Modify the Tony Stark record to change the --
+UPDATE public.account SET account_type Admin 
+WHERE account_id = 1;
 
--- Modify the "GM Hummer" record to read "spacious interior" rather than "small interior" using a single query.--
-UPDATE inventory SET inv_description = REPLACE(inv_description, 'small', 'spacious')
-WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+-- Delete Tony Stark account from database --
+DELETE FROM public.account WHERE account_id = 1;
+
+-- Modify the "GM Hummer" record to read "a huge interior" rather than "small interior" using a single query.--
+UPDATE public.inventory SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+-- In join Sport --
+SELECT inv_make, inv_model
+FROM public.inventory
+INNER JOIN public.classification
+ON inventory.classification_id = classification.classification_id
+WHERE classification_name = 'Sport';
+
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE (inv_image, '/images/', './images/vehicles'),
+    inv_thumbnail = REPLACE (inv_thumbnail, '/images/', './images/vehicles/');
+
+
+
