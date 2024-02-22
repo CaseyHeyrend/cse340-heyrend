6c07c704af9cbfcc5f8e1b25ebe8d0ef87836637
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2024-02-21 22:52:49 -0700

    meh

---
+++ routes/errorRoute.js
@@ -0,0 +1,9 @@
+// Needed resources
+const express = require("express")
+const router = new express.Router()
+const errorController = require("../controllers/errorController")
+
+// Route to generate error page
+router.get("/intentional", errorController.generateIntentionalErrorPage);
+
+module.exports = router;
