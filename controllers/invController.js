449a8ef01c18610e0e3a093da815550c521cc382
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2024-02-22 18:46:24 -0700

    mistake was detail to details

--- controllers/invController.js
+++ controllers/invController.js
@@ -29,7 +29,7 @@ invCont.buildVehicleDetailView = async function (req, res, next) {
   const inv_year = data.inv_year
   const inv_make = data.inv_make
   const inv_model = data.inv_model
-  res.render("./inventory/detail", {
+  res.render("./inventory/details", {
     title: inv_year + " " + inv_make + " " + inv_model,
     nav,
     wrap,
