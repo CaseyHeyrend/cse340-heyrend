a1239a7bea262bc849bd49e9a8e3d3d83cfd6f49
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2024-03-06 20:40:47 -0700

    Spacing

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
-
+// Week 8
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -58,22 +58,29 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(utilities.handleErrors(static))
 //app.get("/", function(req, res){res.render("index", {title: "Home"})})
 //app.get("/", baseController.buildHome)
+
 //Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
+
 // Inventory routes
 app.use("/inv", utilities.handleErrors(inventoryRoute))
+
 // Intentional error route
 app.use("/errors", utilities.handleErrors(errorRoute))
+
 // Account routes
 app.use("/account", utilities.handleErrors(accountRoute))
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
 /* ***********************
 * CRITICAL!
 *"Place after all other middleware"
 ***********************/
+
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
@@ -87,6 +94,7 @@ app.use(async (err, req, res, next) => {
     nav
   })
 })
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
