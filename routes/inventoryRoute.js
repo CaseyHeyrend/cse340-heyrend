d658f1fb1a246695f69ab7ee1ae1cc95c0dcece4
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2024-03-09 01:05:14 -0700

    What is Post in routes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,8 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/");
+const regValidate = require("../utilities/inv-validation");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -9,6 +11,15 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 // Route to build vehicle detail view
 router.get("/detail/:vehicleId", invController.buildVehicleDetailView);
 
-router.get
+// Route to build management view
+router.get("/management", invController.buildManagement);
+
+// Route to build add-classification view
+router.get("/add-classification", invController.buildAddClassification);
+
+// Add-Inventory View
+router.get("/add-inventory",utilities.handleErrors (invController.BuildAddInventory),);
+
+
 
 module.exports = router;
