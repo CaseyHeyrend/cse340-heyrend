80dee05af8b84a257064dc6c79018b62d3d883c5
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2024-03-23 21:11:13 -0600

    no error so far cause I am get this app listening on localhost:5500

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -12,7 +12,6 @@ async function buildLogin(req, res, next) {
     res.render("account/login", {
       title: "Login",
       nav,
-      errors: null,
     })
   }
 
@@ -27,20 +26,6 @@ async function buildRegister(req, res, next) {
     errors: null,
   })
 }
- 
-/* ************************************
- * Deliver Account view
- * Unit 5 deliver account team-activity
- * ***********************************/
-async function buildManagement(req, res, next) {
-  let nav = await utilities.getNav()
-  res.render("account/management", {
-    title: "Account Management",
-    nav,
-    errors: null,
-  })
-}
-
 /* ****************************************
 *  Process Registration
 * *************************************** */
@@ -53,42 +38,50 @@ async function registerAccount(req, res) {
   try {
     // regular password and cost (salt is generated automatically)
     hashedPassword = await bcrypt.hashSync(account_password, 10)
+
+    const regResult = await accountModel.registerAccount(
+      account_firstname,
+      account_lastname,
+      account_email,
+      hashedPassword
+    )
+
+    if (regResult) {
+      req.flash(
+        "notice",
+        `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+      )
+      res.status(201).render("account/login", {
+        title: "Login",
+        nav,
+      })
+    } else {
+      req.flash("error", "Sorry, the registration failed.")
+      res.status(501).render("account/register", {
+        title: "Registration",
+        nav,
+        errors: null,
+      })
+    }
   } catch (error) {
-    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    req.flash("error", "Sorry, there was an error processing the registration.")
     res.status(500).render("account/register", {
       title: "Registration",
       nav,
-      errors: null,
-    })
-  }
-
-  const regResult = await accountModel.registerAccount(
-    account_firstname,
-    account_lastname,
-    account_email,
-    hashedPassword
-  )
-
-  if (regResult) {
-    req.flash(
-      "notice",
-      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-    )
-    res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-      errors: null,
-    })
-  } else {
-    req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
-      title: "Registration",
-      nav,
-      errors: null,
     })
   }
 }
-
+/* ************************************
+ * Deliver Account view
+ * Unit 5 deliver account team-activity
+ * ***********************************/
+async function buildManagement(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/management", {
+    title: "Account Management",
+    nav,
+  })
+}
 
 /* ****************************************
  *  Process login request
@@ -122,7 +115,31 @@ async function accountLogin(req, res) {
     return new Error("Access Forbidden")
   }
 }
-
-
-
-module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildManagement,}
+// logout 
+async function logoutUser(req, res, next) {
+  utilities.logout(req, res, next)
+  req.flash("notice", "You're now logged out.")
+  return res.redirect("/")
+}
+// update
+async function buildAccountEdit(req, res, next) {
+  let nav = await utilities.getNav()
+  const account_id = parseInt(req.params.account_id)
+  const accountData = await accountModel.getAccountByID(account_id)
+  try {
+    res.render("./account/update", {
+      title: "Edit Account",
+      nav,
+      errors: null,
+      account_id: accountData.account_id,
+      account_firstname: accountData.account_firstname,
+      account_lastname: accountData.account_lastname,
+      account_email: accountData.account_email,
+    })
+  } catch (error) {
+    error.status = 500
+    console.error(error.status)
+    next(error)
+  }
+}
+module.exports = { buildLogin, buildRegister, registerAccount, accountLogin, buildManagement, logoutUser, buildAccountEdit,}
--- models/account-model.js
+++ models/account-model.js
@@ -37,5 +37,31 @@ async function getAccountByEmail (account_email) {
     return new Error("No matching email found")
   }
 }
-  
-module.exports = { registerAccount, checkExistingEmail, getAccountByEmail, }
+// Account ID
+async function getAccountByID(account_id) {
+  try {
+    const result = await pool.query(
+      "SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_id = $1",
+      [account_id]
+    )
+    return result.rows[0]
+  } catch (error) {
+    return new Error("No matching id found")
+  }
+}
+// Update Account
+async function updateAccount(account_id, account_firstname, account_lastname, account_email) 
+{
+  try {
+    const sql =
+      "UPDATE public.account SET  account_firstname = $1, account_lastname = $2, account_email = $3 WHERE account_id = $4 RETURNING *"
+
+    const data = await pool.query(sql, [ account_firstname, account_lastname, account_email, account_id,])
+    return data.rows[0]
+  } catch (error) {
+    console.error("account-model/updateAccount error: " + error)
+  }
+}
+
+
+module.exports = { registerAccount, checkExistingEmail, getAccountByEmail, getAccountByID, updateAccount, }
